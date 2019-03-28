98810501ef57e104b24e29ea574a86732e7836e1
itaybre <itay.brenner@mercadolibre.com>
2019-03-28 12:29:57 -0300

    Delete Key instead of changing value

--- lib/cocoapods/validator.rb
+++ lib/cocoapods/validator.rb
@@ -555,10 +555,8 @@ module Pod
       app_project = Xcodeproj::Project.new(validation_dir + 'App.xcodeproj')
       app_target = Pod::Generator::AppTargetHelper.add_app_target(app_project, consumer.platform_name, deployment_target)
       Pod::Generator::AppTargetHelper.add_swift_version(app_target, derived_swift_version)
-      app_project.targets.each do |target|
-        target.build_configurations.each do |config|
-          config.build_settings['ASSETCATALOG_COMPILER_APPICON_NAME'] ||= ''
-        end
+      app_target.build_configurations.each do |config|
+        config.build_settings.delete('ASSETCATALOG_COMPILER_APPICON_NAME')
       end
 
       app_project.save
